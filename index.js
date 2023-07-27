67d2dc92b374dc654bfb2330baa10c415b59eb78
Ritika Kumar <kumarritika666@gmail.com>
2023-07-28 04:28:21 +1400

    pre-final touch

---
+++ index.js
@@ -0,0 +1,31 @@
+const btnopen = document.querySelectorAll(".show-modal");
+console.log(btnopen);
+
+const btnclose = document.querySelector(".close-modal");
+console.log(btnclose);
+
+const modal = document.querySelector(".modal");
+console.log(modal);
+
+const overlay = document.querySelector(".overlay");
+console.log(overlay);
+
+const open = function (event) {
+  const btn = event.target.dataset.text;
+  const modalText = document.querySelector(".modal p");
+  modalText.textContent = btn;
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const close = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnopen.length; i++) {
+  btnopen[i].addEventListener("click", open);
+  overlay.addEventListener("click", close);
+}
+
+btnclose.addEventListener("click", close);
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,19 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
-        '128': '46rem',
-      }
+        110: "36rem",
+      },
+
+      boxShadow: {
+        "inset-transparent": "inset 0 0 0 1000px rgba(0, 0, 0, 0.2)",
+      },
+
+      fontFamily: {
+        // Add your custom font family here
+        custom: ["Comfortaa", "cursive"],
+        info: ["Poppins", "sans-serif"],
+      },
     },
   },
-  plugins: [
-    require('preline/plugin'),
-  ],
-}
-
+  plugins: [require("preline/plugin")],
+};
